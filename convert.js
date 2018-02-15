88ac32af80cb23281d52a066bde120ba3e86d32f
Ievgenii Shepeliuk <eshepelyuk@newagesol.com>
2018-02-16 00:20:35 +0200

    npm skeleton

--- convert.js
+++
@@ -1,8 +0,0 @@
-const asciidoctor = require('asciidoctor.js')();
-
-const AsciidoctorJS_PlantUML = require('./asciidoctorjs-plantuml.js');
-asciidoctor.Extensions.register(AsciidoctorJS_PlantUML);
-
-const html = asciidoctor.convertFile('document-plantuml.adoc', 
-    { to_file: false, header_footer: true });
-console.log(html);

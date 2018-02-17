ccfe46019edb71d080204275962ddb0176e74775
Ievgenii Shepeliuk <eshepelyuk@newagesol.com>
2018-02-17 11:03:39 +0200

    examples added

---
+++ examples/convert.js
@@ -0,0 +1,8 @@
+const asciidoctor = require('asciidoctor.js')();
+
+const AsciidoctorJS_PlantUML = require('asciidoctorjs-plantuml');
+asciidoctor.Extensions.register(AsciidoctorJS_PlantUML);
+
+const html = asciidoctor.convertFile('document-plantuml.adoc', 
+    { to_file: false  });
+console.log(html);

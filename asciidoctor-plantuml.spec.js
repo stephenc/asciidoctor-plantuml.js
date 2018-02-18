3c6df57213caf6e0cb2f2d7a388e4d0a197da2e6
Ievgenii Shepeliuk <eshepelyuk@newagesol.com>
2018-02-18 15:21:30 +0200

    more tests

--- asciidoctor-plantuml.spec.js
+++ asciidoctor-plantuml.spec.js
@@ -1,4 +1,13 @@
+
 describe("Asciidoctor PlantUML", function () {
+    const DOCUMENT = `
+[plantuml]
+----
+@startuml
+alice -> bob
+@enduml
+----
+`;
 
     const asciidoctor = require('asciidoctor.js')();
 
@@ -21,6 +30,21 @@ describe("Asciidoctor PlantUML", function () {
 
             plantuml.register(registry);
             expect(registeredForBlock()).not.toBe(null);
+        });
+    });
+
+    describe("PlantUML server URL attribute", function () {
+
+        let registry;
+
+        beforeAll(function () {
+            registry = asciidoctor.Extensions.create();
+            plantuml.register(registry);
+        });
+
+        it("should not set attribute if not declared", function () {
+            const doc = asciidoctor.load("== Header2");
+            expect(doc.getAttribute("plantuml-server-url")).toBe(undefined);
         });
     });
 

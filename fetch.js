7cee33f3fffd81a01fe2013dffcc6d21612b98ce
Evgeny Shepelyuk <eshepelyuk@newagesol.com>
2018-02-20 10:33:40 +0200

    info about detecting image store path

--- fetch.js
+++ fetch.js
@@ -1,3 +1,34 @@
+/*
+
+Image Output Location
+
+When Asciidoctor Diagram writes images to disk it will go over the following options in order to determine where to write the files.
+
+{imagesoutdir} if the imagesoutdir attribute has been specified
+
+{outdir}/{imagesdir} if the outdir attribute has been specified
+
+{to_dir}/{imagesdir} if the to_dir attribute has been specified
+
+{base_dir}/{imagesdir}
+
+        def image_output_dir(parent)
+          document = parent.document
+
+          images_dir = parent.attr('imagesoutdir')
+
+          if images_dir
+            base_dir = nil
+          else
+            base_dir = parent.attr('outdir') || (document.respond_to?(:options) && document.options[:to_dir])
+            images_dir = parent.attr('imagesdir')
+          end
+
+          parent.normalize_system_path(images_dir, base_dir)
+        end
+    
+    */
+
 const DIAGRAM = `@startuml
 alice -> bob
 @enduml

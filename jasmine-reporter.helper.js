c072ff5f9f676bce8ed8bc266890fb175e5ae4a9
Ievgenii Shepeliuk <eshepelyuk@newagesol.com>
2018-02-23 23:58:12 +0200

    show error if PlantUML server URL is not defined

--- jasmine-reporter.helper.js
+++ jasmine-reporter.helper.js
@@ -1,7 +1,7 @@
 const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
-jasmine.getEnv().clearReporters();               // remove default reporter logs
-jasmine.getEnv().addReporter(new SpecReporter({  // add jasmine-spec-reporter
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(new SpecReporter({
     spec: {
         displayPending: true,
         displayStacktrace: true

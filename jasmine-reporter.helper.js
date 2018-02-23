8f56f8ef98cd18b767c7da8abb866f065df22bda
Ievgenii Shepeliuk <eshepelyuk@newagesol.com>
2018-02-23 22:11:26 +0200

    better test reporting

--- jasmine-reporter.helper.js
+++ jasmine-reporter.helper.js
@@ -1,11 +1,9 @@
-// jasmine.getEnv().clearReporters();
-//
-// const JasmineConsoleReporter = require('jasmine-console-reporter');
-// const reporter = new JasmineConsoleReporter({
-//     colors: 1,           // (0|false)|(1|true)|2
-//     cleanStack: 1,       // (0|false)|(1|true)|2|3
-//     verbosity: 4,        // (0|false)|1|2|(3|true)|4
-//     listStyle: 'indent', // "flat"|"indent"
-//     activity: false
-// });
-// jasmine.getEnv().addReporter(reporter);
+const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
+jasmine.getEnv().clearReporters();               // remove default reporter logs
+jasmine.getEnv().addReporter(new SpecReporter({  // add jasmine-spec-reporter
+    spec: {
+        displayPending: true,
+        displayStacktrace: true
+    }
+}));

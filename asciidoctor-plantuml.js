c499683733aae0cbf0470187c2d592c2db9e6bbf
Evgeny Shepelyuk <eshepelyuk@newagesol.com>
2018-03-19 14:51:21 +0200

    Standard.js

--- asciidoctor-plantuml.js
+++ asciidoctor-plantuml.js
@@ -1,14 +1,14 @@
 /* global Opal */
-const plantumlEncoder = require('plantuml-encoder');
-const request = require('sync-request');
-const fs = require('fs');
-const randomstring = require('randomstring');
-const path = require('path');
-const mkdirp = require('mkdirp');
+const plantumlEncoder = require('plantuml-encoder')
+const request = require('sync-request')
+const fs = require('fs')
+const randomstring = require('randomstring')
+const path = require('path')
+const mkdirp = require('mkdirp')
 
 function createPlantumlBlock (parent, content, attrs) {
-  const opts = Opal.hash({content_model: 'raw', source: content, subs: 'default'}).$merge(attrs);
-  return Opal.Asciidoctor.Block.$new(parent, 'pass', opts);
+  const opts = Opal.hash({content_model: 'raw', source: content, subs: 'default'}).$merge(attrs)
+  return Opal.Asciidoctor.Block.$new(parent, 'pass', opts)
 }
 
 function plantumlNoServerURLContent () {
@@ -17,59 +17,58 @@ function plantumlNoServerURLContent () {
 <pre class="plantuml plantuml-error">PlantUML Server URL is not defined neither via :plantuml-server-url: nor via PLANTUML_SERVER_URL environment variable</pre>
 </div>
 </div>
-`;
+`
 }
 
 function createImageTag (serverUrl, shouldFetch, outdir, text, blockId) {
-  let diagramUrl = `${serverUrl}/png/${plantumlEncoder.encode(text)}`;
+  let diagramUrl = `${serverUrl}/png/${plantumlEncoder.encode(text)}`
   if (shouldFetch) {
-    mkdirp.sync(outdir || '');
-    const diagramName = `${randomstring.generate()}.png`;
-    const diagramPath = path.format({dir: outdir || '', base: diagramName});
+    mkdirp.sync(outdir || '')
+    const diagramName = `${randomstring.generate()}.png`
+    const diagramPath = path.format({dir: outdir || '', base: diagramName})
 
-    fs.writeFileSync(diagramPath, request('GET', diagramUrl).getBody());
-    diagramUrl = diagramName;
+    fs.writeFileSync(diagramPath, request('GET', diagramUrl).getBody())
+    diagramUrl = diagramName
   }
-  let content = '<img ';
-  content += (blockId ? `data-pumlid="${blockId}" ` : '');
-  content += `class="plantuml" src="${diagramUrl}"/>`;
-  return content;
+  let content = '<img '
+  content += (blockId ? `data-pumlid="${blockId}" ` : '')
+  content += `class="plantuml" src="${diagramUrl}"/>`
+  return content
 }
 
 function plantumlBlock () {
-  this.named('plantuml');
-  this.onContext('listing');
+  this.named('plantuml')
+  this.onContext('listing')
   this.positionalAttributes('id');
 
   this.process(function (parent, reader, attrs) {
-    const serverUrl = process.env.PLANTUML_SERVER_URL || parent.getDocument().getAttribute('plantuml-server-url');
-    let content;
+    const serverUrl = process.env.PLANTUML_SERVER_URL || parent.getDocument().getAttribute('plantuml-server-url')
+    let content
     if (serverUrl) {
-      const shouldFetch = parent.getDocument().isAttribute('plantuml-fetch-diagram');
+      const shouldFetch = parent.getDocument().isAttribute('plantuml-fetch-diagram')
       content = `
 <div class="imageblock">
 <div class="content">
-${createImageTag(serverUrl, shouldFetch, 
-    parent.getDocument().getAttribute('imagesoutdir'), 
-    reader.getLines().join('\n'), 
+${createImageTag(serverUrl, shouldFetch,
+    parent.getDocument().getAttribute('imagesoutdir'),
+    reader.getLines().join('\n'),
     Opal.hash_get(attrs, 'id'))}
 </div>
-</div>`;
+</div>`
     } else {
-      content = plantumlNoServerURLContent();
+      content = plantumlNoServerURLContent()
     }
-    return createPlantumlBlock(parent, content, attrs);
-  });
+    return createPlantumlBlock(parent, content, attrs)
+  })
 }
 
 module.exports.register = function register (registry) {
   if (typeof registry.register === 'function') {
     registry.register(function () {
-      this.block(plantumlBlock);
-    });
+      this.block(plantumlBlock)
+    })
   } else if (typeof registry.block === 'function') {
-    registry.block('plantuml', plantumlBlock);
+    registry.block('plantuml', plantumlBlock)
   }
-  return registry;
-};
-
+  return registry
+}
--- asciidoctor-plantuml.spec.js
+++ asciidoctor-plantuml.spec.js
@@ -1,45 +1,45 @@
 /* eslint-env jasmine */
 /* global Opal */
-const LOCAL_URL = 'http://localhost:8080';
-const PLANTUML_REMOTE_URL = 'http://www.plantuml.com/plantuml';
+const LOCAL_URL = 'http://localhost:8080'
+const PLANTUML_REMOTE_URL = 'http://www.plantuml.com/plantuml'
 const DIAGRAM = `@startuml
 alice -> bob
 @enduml
-`;
+`
 
-const DIAGRAM_SIZE = 1788;
+const DIAGRAM_SIZE = 1788
 
-const plantuml = require('./asciidoctor-plantuml.js');
-const asciidoctor = require('asciidoctor.js')();
-const fs = require('fs');
-const tmp = require('tmp');
-const path = require('path');
+const plantuml = require('./asciidoctor-plantuml.js')
+const asciidoctor = require('asciidoctor.js')()
+const fs = require('fs')
+const tmp = require('tmp')
+const path = require('path')
 
-tmp.setGracefulCleanup();
+tmp.setGracefulCleanup()
 
 describe('extension registration', function () {
+  let registry
 
-  let registry;
+  let registeredForBlock = () => Opal.send(registry, 'registered_for_block?', ['plantuml', 'listing'])
 
-  let registeredForBlock = () => Opal.send(registry, 'registered_for_block?', ['plantuml', 'listing']);
-
-  beforeAll(() => registry = asciidoctor.Extensions.create());
+  beforeAll(function () {
+    registry = asciidoctor.Extensions.create()
+  })
 
   it('should register plantuml block for listing ctx', () => {
-    expect(registeredForBlock).toThrowError(/undefined method/);
+    expect(registeredForBlock).toThrowError(/undefined method/)
 
-    plantuml.register(registry);
-    expect(registeredForBlock()).not.toBeNull();
-  });
-});
+    plantuml.register(registry)
+    expect(registeredForBlock()).not.toBeNull()
+  })
+})
 
 describe('conversion to HTML', () => {
+  const cheerio = require('cheerio')
 
-  const cheerio = require('cheerio');
+  const plantumlEncoder = require('plantuml-encoder')
 
-  const plantumlEncoder = require('plantuml-encoder');
-
-  const $$ = (doc) => cheerio.load(asciidoctor.convert(doc, {extension_registry: registry}));
+  const $$ = (doc) => cheerio.load(asciidoctor.convert(doc, {extension_registry: registry}))
 
   const ADOC = (docAttrs = [], blockAttrs = []) => {
     return `            
@@ -48,119 +48,119 @@ ${docAttrs.join('\n')}
 ----
 ${DIAGRAM}
 ----
-`;
-  };
+`
+  }
 
-  let registry, encodedDiagram;
+  let registry, encodedDiagram
 
   beforeAll(() => {
-    registry = plantuml.register(asciidoctor.Extensions.create());
-    encodedDiagram = plantumlEncoder.encode(DIAGRAM);
-  });
+    registry = plantuml.register(asciidoctor.Extensions.create())
+    encodedDiagram = plantumlEncoder.encode(DIAGRAM)
+  })
 
-  afterEach(() => process.env.PLANTUML_SERVER_URL = '');
+  afterEach(function () {
+    process.env.PLANTUML_SERVER_URL = ''
+  })
 
   it('should create div[class=imageblock] with img[class=plantuml] inside', () => {
-    const root = $$(ADOC([`:plantuml-server-url: ${LOCAL_URL}`]))('div.imageblock');
-    expect(root.find('div.content img.plantuml').length).toBe(1);
-  });
+    const root = $$(ADOC([`:plantuml-server-url: ${LOCAL_URL}`]))('div.imageblock')
+    expect(root.find('div.content img.plantuml').length).toBe(1)
+  })
 
   describe('diagram attributes', () => {
     it('should populate img.data-pumlid from named attr', () => {
-      const img = $$(ADOC([`:plantuml-server-url: ${LOCAL_URL}`], ['id=myId']))('img.plantuml');
-      expect(img.data('pumlid')).toBe('myId');
-    });
+      const img = $$(ADOC([`:plantuml-server-url: ${LOCAL_URL}`], ['id=myId']))('img.plantuml')
+      expect(img.data('pumlid')).toBe('myId')
+    })
 
     it('should populate img.data-pumlid from positional attr', () => {
-      const img = $$(ADOC([`:plantuml-server-url: ${LOCAL_URL}`], ['myId']))('img.plantuml');
-      expect(img.data('pumlid')).toBe('myId');
-    });
-  });
+      const img = $$(ADOC([`:plantuml-server-url: ${LOCAL_URL}`], ['myId']))('img.plantuml')
+      expect(img.data('pumlid')).toBe('myId')
+    })
+  })
 
   describe('PlantUML server URL', () => {
     it('should use :plantuml-server-url: for diagram src', () => {
-      const src = $$(ADOC([`:plantuml-server-url: ${LOCAL_URL}`]))('img.plantuml').attr('src');
-      expect(src).toBe(`${LOCAL_URL}/png/${encodedDiagram}`);
-    });
+      const src = $$(ADOC([`:plantuml-server-url: ${LOCAL_URL}`]))('img.plantuml').attr('src')
+      expect(src).toBe(`${LOCAL_URL}/png/${encodedDiagram}`)
+    })
 
     it('when :plantuml-server-url: missing then use PLANTUML_SERVER_URL', () => {
-      process.env.PLANTUML_SERVER_URL = PLANTUML_REMOTE_URL;
-      const src = $$(ADOC())('img.plantuml').attr('src');
-      expect(src).toBe(`${PLANTUML_REMOTE_URL}/png/${encodedDiagram}`);
-    });
+      process.env.PLANTUML_SERVER_URL = PLANTUML_REMOTE_URL
+      const src = $$(ADOC())('img.plantuml').attr('src')
+      expect(src).toBe(`${PLANTUML_REMOTE_URL}/png/${encodedDiagram}`)
+    })
 
     it('PLANTUML_SERVER_URL should override :plantuml-server-url:', () => {
-      process.env.PLANTUML_SERVER_URL = PLANTUML_REMOTE_URL;
-      const src = $$(ADOC([`:plantuml-server-url: ${LOCAL_URL}`]))('img.plantuml').attr('src');
-      expect(src).toBe(`${PLANTUML_REMOTE_URL}/png/${encodedDiagram}`);
-    });
+      process.env.PLANTUML_SERVER_URL = PLANTUML_REMOTE_URL
+      const src = $$(ADOC([`:plantuml-server-url: ${LOCAL_URL}`]))('img.plantuml').attr('src')
+      expect(src).toBe(`${PLANTUML_REMOTE_URL}/png/${encodedDiagram}`)
+    })
 
     it('should generate HTML error when no :plantuml-server-url: and no PLANTUML_SERVER_URL', () => {
-      const rootDiv = $$(ADOC())('div.listingblock');
-      expect(rootDiv.find('img').length).toBe(0);
-      expect(rootDiv.find('div.plantuml-error')).toBeDefined();
-      expect(rootDiv.text()).toContain('PlantUML Server URL is not defined');
-    });
-  });
+      const rootDiv = $$(ADOC())('div.listingblock')
+      expect(rootDiv.find('img').length).toBe(0)
+      expect(rootDiv.find('div.plantuml-error')).toBeDefined()
+      expect(rootDiv.text()).toContain('PlantUML Server URL is not defined')
+    })
+  })
 
   describe('diagram fetching', () => {
-    let src = undefined;
+    let src
 
     afterEach(() => {
       try {
-        fs.unlinkSync(src);
+        fs.unlinkSync(src)
       } catch (e) {
         // ignore
       }
-    });
+    })
 
     it('should fetch when :plantuml-fetch-diagram: set', () => {
-      const html = $$(ADOC([`:plantuml-server-url: ${PLANTUML_REMOTE_URL}`, ':plantuml-fetch-diagram:']));
+      const html = $$(ADOC([`:plantuml-server-url: ${PLANTUML_REMOTE_URL}`, ':plantuml-fetch-diagram:']))
 
-      src = html('img.plantuml').attr('src');
+      src = html('img.plantuml').attr('src')
 
-      expect(src).toEndWith('.png');
+      expect(src).toEndWith('.png')
 
-      expect(fs.existsSync(src)).toBe(true);
-      expect(fs.statSync(src).size).toBe(DIAGRAM_SIZE);
-    });
+      expect(fs.existsSync(src)).toBe(true)
+      expect(fs.statSync(src).size).toBe(DIAGRAM_SIZE)
+    })
 
     it('should support :imagesoutdir: for storing images', () => {
-      const tmpDir = tmp.dirSync({prefix: 'adoc_puml_'});
+      const tmpDir = tmp.dirSync({prefix: 'adoc_puml_'})
 
       const html = $$(ADOC([`:plantuml-server-url: ${PLANTUML_REMOTE_URL}`,
         ':plantuml-fetch-diagram:',
         `:imagesoutdir: ${tmpDir.name}`
-      ]));
+      ]))
 
-      src = html('img.plantuml').attr('src');
+      src = html('img.plantuml').attr('src')
 
-      expect(src).toEndWith('.png');
+      expect(src).toEndWith('.png')
 
-      const diagramPath = path.format({dir: tmpDir.name, base: src});
+      const diagramPath = path.format({dir: tmpDir.name, base: src})
 
-      expect(fs.existsSync(diagramPath)).toBe(true);
-      expect(fs.statSync(diagramPath).size).toBe(DIAGRAM_SIZE);
-    });
+      expect(fs.existsSync(diagramPath)).toBe(true)
+      expect(fs.statSync(diagramPath).size).toBe(DIAGRAM_SIZE)
+    })
 
     it('should create nested subdirectories of :imagesoutdir:', () => {
-      const missingDir = path.join(tmp.dirSync({prefix: 'adoc_puml_'}).name, 'missing', 'dir');
+      const missingDir = path.join(tmp.dirSync({prefix: 'adoc_puml_'}).name, 'missing', 'dir')
 
       const html = $$(ADOC([`:plantuml-server-url: ${PLANTUML_REMOTE_URL}`,
         ':plantuml-fetch-diagram:',
         `:imagesoutdir: ${missingDir}`
-      ]));
+      ]))
 
-      src = html('img.plantuml').attr('src');
+      src = html('img.plantuml').attr('src')
 
-      expect(src).toEndWith('.png');
+      expect(src).toEndWith('.png')
 
-      const diagramPath = path.format({dir: missingDir, base: src});
+      const diagramPath = path.format({dir: missingDir, base: src})
 
-      expect(fs.existsSync(diagramPath)).toBe(true);
-      expect(fs.statSync(diagramPath).size).toBe(DIAGRAM_SIZE);
-    });
-  });
-
-});
-
+      expect(fs.existsSync(diagramPath)).toBe(true)
+      expect(fs.statSync(diagramPath).size).toBe(DIAGRAM_SIZE)
+    })
+  })
+})

bfce46aa303fd375136f262d28a0a61700057ce9
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2018-04-14 09:54:37 +0300

    fix [skip ci]

--- script/milestone_changelog.sh
+++ script/milestone_changelog.sh
@@ -5,4 +5,5 @@ loghub -t $GITHUB_TOKEN -f release -m $TRAVIS_TAG \
   -ilg infrastructure "Project infrastructure" \
   -ilg feature "New functionality" \
   --template script/milestone_changelog.j2 \
-  --no-prs "$TRAVIS_REPO_SLUG" | sed -n -e '/^@@@@$/,/^@@@@$/{ /^@@@@$/d; /^@@@@$/d; p; }'
+  --no-prs "$TRAVIS_REPO_SLUG"
+#  | sed -n -e '/^@@@@$/,/^@@@@$/{ /^@@@@$/d; /^@@@@$/d; p; }'

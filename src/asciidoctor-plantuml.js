68eab61141e9342496ebd4d9b5dfa93fa889857e
Guillaume Grossetie <g.grossetie@gmail.com>
2018-11-05 22:38:17 +0100

    Compatibility with Asciidoctor.js 1.5.7 (#31)
    Make the extension compatible with Asciidoctor.js 1.5.6 and 1.5.7

--- src/asciidoctor-plantuml.js
+++ src/asciidoctor-plantuml.js
@@ -1,5 +1,17 @@
-/* global Opal */
 const plantumlEncoder = require('plantuml-encoder')
+
+/**
+ * Convert an (Opal) Hash to JSON.
+ * @private
+ */
+const fromHash = function (hash) {
+  const object = {}
+  const data = hash.$$smap
+  for (let key in data) {
+    object[key] = data[key]
+  }
+  return object
+}
 
 function serverUnavailableBlock (processor, parent, context, source, attrs) {
   return processor.createBlock(parent, context, source, attrs)
@@ -21,15 +33,18 @@ function plantumlBlock () {
   this.positionalAttributes(['target', 'format'])
 
   this.process((parent, reader, attrs) => {
+    if (typeof attrs === 'object' && '$$smap' in attrs) {
+      attrs = fromHash(attrs)
+    }
     const doc = parent.getDocument()
     const diagramText = reader.getString()
     const serverUrl = doc.getAttribute('plantuml-server-url')
-    const role = Opal.hash_get(attrs, 'role')
-    const blockId = Opal.hash_get(attrs, 'id')
+    const role = attrs.role
+    const blockId = attrs.id
 
     if (serverUrl) {
-      const target = Opal.hash_get(attrs, 'target')
-      const format = Opal.hash_get(attrs, 'format') || 'png'
+      const target = attrs.target
+      const format = attrs.format || 'png'
       if (format === 'png' || format === 'svg') {
         const imageUrl = createImageSrc(doc, diagramText, target, format)
         const blockAttrs = {role: role ? `${role} plantuml` : 'plantuml', target: imageUrl, alt: target || 'diagram'}
@@ -37,13 +52,13 @@ function plantumlBlock () {
         return this.createImageBlock(parent, blockAttrs)
       } else {
         console.warn(`Skipping plantuml block. Format ${format} is unsupported by PlantUML`)
-        Opal.hash_put(attrs, 'role', role ? `${role} plantuml-error` : 'plantuml-error')
-        return serverUnavailableBlock(this, parent, Opal.hash_get(attrs, 'cloaked-context'), diagramText, attrs)
+        attrs.role = role ? `${role} plantuml-error` : 'plantuml-error'
+        return serverUnavailableBlock(this, parent, attrs['cloaked-context'], diagramText, attrs)
       }
     } else {
       console.warn('Skipping plantuml block. PlantUML Server URL not defined in :plantuml-server-url: attribute.')
-      Opal.hash_put(attrs, 'role', role ? `${role} plantuml-error` : 'plantuml-error')
-      return serverUnavailableBlock(this, parent, Opal.hash_get(attrs, 'cloaked-context'), diagramText, attrs)
+      attrs.role = role ? `${role} plantuml-error` : 'plantuml-error'
+      return serverUnavailableBlock(this, parent, attrs['cloaked-context'], diagramText, attrs)
     }
   })
 }
